f85bf8d02e9eed9c2fc0d23ff820d89d16467295
airy <airy@gaia.(none)>
2009-09-01 23:23:24 +0300

    need code for sending event to server

--- src/demo_level.rb
+++ src/demo_level.rb
@@ -1,9 +1,12 @@
 require 'level'
+
 class DemoLevel < Level
   def setup
     @toolbar = create_actor :toolbar, :x => 30, :y => 600
     @toolbar.when :cast_spell do |spell|
         puts "THEY JUST CAST!  [#{spell}]"
+	#here there must be a code for sending event to server like
+	#$game_client.send_data "casting [#{spell}]"
     end
   end
 
