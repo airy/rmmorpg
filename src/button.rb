a545c0e17590ddf90b57bebb511a6c9d8bbd3f43
Shawn Anderson <shawn42@gmail.com>
2009-09-01 08:55:06 -0400

    added test to the button

--- src/button.rb
+++ src/button.rb
@@ -3,9 +3,19 @@ require 'actor_view'
 require 'publisher'
 
 class ButtonView < ActorView
+  def setup
+    @font = @actor.resource_manager.load_font 'porky.ttf', 30
+    @image = @font.render @actor.name, true, [250,250,250]
+  end
+
   def draw(target, x_off, y_off)
     # paint it red for now
-    target.draw_box_s [x_off+@actor.x,y_off+@actor.y], [x_off+@actor.x+@actor.w,y_off+@actor.y+@actor.h], [250,0,0, 255]
+    x = x_off+@actor.x
+    y = y_off+@actor.y
+    target.draw_box_s [x,y], [x+@actor.w,y+@actor.h], [250,0,0, 255]
+
+    @image.blit target.screen, [x,y]
+    
     if @actor.cooling_down?
       target.draw_box_s [x_off+@actor.x,y_off+@actor.y], [x_off+@actor.x+@actor.w,y_off+@actor.y+@actor.h], [0,0,0, 155]
     end
@@ -22,13 +32,14 @@ class Button < Actor
   has_behaviors :updatable,
     :layered => 3
 
-  attr_accessor :w, :h
+  attr_accessor :w, :h, :name
   def setup
     # register for events here
     # or pull stuff out of @opts
     @w = 60
     @h = 60
     @cooling_down = 0
+    @name = "WIN"
 
 
     i = input_manager
